3ccda987c7239a81f16898afb86efab060f6ee4c
AbelJs002 <abeljs007@gmail.com>
2026-09-13 19:26:57 +0700

    Pengumpulan Tugas 3

---
+++ tugas3_IfnLoop.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+import 'dart:math';
+void main () {
+  // No. 1
+// stdout.write("Masukkan nilai: ");
+//   int nilai = int.parse(stdin.readLineSync()!);
+
+//   if (nilai >= 85) {
+//     print("Nilai: A");
+//   } else if (nilai >= 70) {
+//     print("Nilai: B");
+//   } else if (nilai >= 55) {
+//     print("Nilai: C");
+//   } else {
+//     print("Nilai: D");
+//   }
+
+
+  // No. 2
+  // for (int i = 1; i <= 20; i++) {
+  //   if (i % 3 == 0 && i % 5 == 0) {
+  //     print("FizzBuzz");
+  //   } else if (i % 3 == 0) {
+  //     print("Fizz");
+  //   } else if (i % 5 == 0) {
+  //     print("Buzz");
+  //   } else {
+  //     print(i);
+  //   }
+  // }
+
+
+  // No. 3
+  // List<int> angka = [];
+
+  // for (int i = 1; i <= 5; i++) {
+  //   stdout.write("Masukkan angka ke-$i: ");
+  //   angka.add(int.parse(stdin.readLineSync()!));
+  // }
+
+  // int jumlah = angka.reduce((a, b) => a + b);
+  // double rata = jumlah / 5;
+  // int maksimum = angka.reduce((a, b) => a > b ? a : b);
+  // int minimum = angka.reduce((a, b) => a < b ? a : b);
+
+  // print("Jumlah: $jumlah");
+  // print("Rata-rata: $rata");
+  // print("Maksimum: $maksimum");
+  // print("Minimum: $minimum");
+
+
+  // No. 4
+  stdout.write("Masukkan angka Anda (0-5): ");
+  int angkaTebakan = int.parse(stdin.readLineSync()!);
+
+  var random = Random();
+  int angkaRandom = random.nextInt(6);
+
+  if (angkaTebakan == angkaRandom) {
+    print("Tebakan benar!");
+  } else {
+    print("Tebakan salah!");
+    print("Angka yang benar: $angkaRandom");
+  }
+}
